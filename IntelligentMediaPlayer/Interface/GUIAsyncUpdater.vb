9c8d9b3a1cc7fa44bcbd31b521bd31d7c00c1454
Jason Antic <jsa169@gmail.com>
2010-02-02 20:58:32 -0800

    Finally figured out, the hard way, that message queues / dedicated daemon threads are the best way to go in terms of asynchronous gui updates.  I've updated the code accordingly- performance is *much* better, and the code is much easier to follow.
    Signed-off-by: Jason Antic <jsa169@gmail.com>

---
+++ IntelligentMediaPlayer/Interface/GUIAsyncUpdater.vb
@@ -0,0 +1,276 @@
+﻿
+Partial Public Class MainInterface
+    Private Class GUIAsyncUpdater
+        Private myArtistNameQueue As New Queue(Of String)
+
+        Private myBiographyArtistInfoQueue As New Queue(Of IArtistInfo)
+        Private myTagsArtistInfoQueue As New Queue(Of IArtistInfo)
+        Private mySimilarArtistInfoQueue As New Queue(Of IArtistInfo)
+        Private myTopAlbumsArtistInfoQueue As New Queue(Of IArtistInfo)
+
+        Dim sleeptime As UInteger = 250 ' ms
+
+
+        Private myParentInterface As MainInterface
+
+        Public Sub New(ByRef theParentInterface As MainInterface)
+            myParentInterface = theParentInterface
+
+            Dim UpdateArtistInfoTHREAD As New System.Threading.Thread(AddressOf UpdateArtistInfo)
+            UpdateArtistInfoTHREAD.Start()
+            Dim UpdateBiographyTHREAD As New System.Threading.Thread(AddressOf UpdateBiography)
+            UpdateBiographyTHREAD.Start()
+            Dim UpdateSimilarArtistsTHREAD As New System.Threading.Thread(AddressOf UpdateSimilarArtists)
+            UpdateSimilarArtistsTHREAD.Start()
+            Dim UpdateTagsTHREAD As New System.Threading.Thread(AddressOf UpdateTags)
+            UpdateTagsTHREAD.Start()
+            Dim UpdateTopAlbumsTHREAD As New System.Threading.Thread(AddressOf UpdateTopAlbums)
+            UpdateTopAlbumsTHREAD.Start()
+        End Sub
+
+        Public Sub AddArtistNameToQueue(ByVal artistName As String)
+            If (myArtistNameQueue.Count = 0) Then
+                myArtistNameQueue.Enqueue(artistName)
+            ElseIf (artistName.Trim.ToLower <> myArtistNameQueue.Peek().Trim.ToLower) Then
+                myArtistNameQueue.Enqueue(artistName)
+            End If
+        End Sub
+
+        Private Sub UpdateArtistInfo()
+            While (True)
+                Dim currentArtist As String = Nothing
+
+                While (myArtistNameQueue.Count > 0) 'don't need/want to update for artist info object that has already expired
+                    currentArtist = myArtistNameQueue.Dequeue()
+                End While
+
+                If (Not currentArtist Is Nothing) Then
+                    Dim artistInfo As IArtistInfo = New LastFMArtistInfo(currentArtist)
+                    myBiographyArtistInfoQueue.Enqueue(artistInfo)
+                    myTagsArtistInfoQueue.Enqueue(artistInfo)
+                    mySimilarArtistInfoQueue.Enqueue(artistInfo)
+                    myTopAlbumsArtistInfoQueue.Enqueue(artistInfo)
+                    System.GC.Collect()
+                End If
+
+                System.Threading.Thread.Sleep(sleeptime)
+            End While
+        End Sub
+
+        Private Sub UpdateTopAlbums()
+            While (True)
+                Dim artistInfo As IArtistInfo = Nothing
+
+                While (myTopAlbumsArtistInfoQueue.Count > 0)  'don't need/want to update for artist info object that has already expired
+                    artistInfo = myTopAlbumsArtistInfoQueue.Dequeue
+                End While
+
+                If (Not artistInfo Is Nothing) Then
+                    If (Not myParentInterface.TopAlbumsLV.LargeImageList Is Nothing) Then
+                        For Each oldImage As Image In myParentInterface.TopAlbumsLV.LargeImageList.Images  'clean up images first!  Otherwise- we get out of memory exceptions.
+                            oldImage.Dispose()
+                        Next
+                    End If
+
+                    myParentInterface.TopAlbumsLV.Clear()
+
+                    Dim albums As IAlbumInfo() = artistInfo.TopAlbums()
+
+                    If (Not albums Is Nothing) Then
+
+                        Dim albumImages As ImageList = New ImageList()
+                        albumImages.ImageSize = GetTopAlbumImagesSize()
+                        albumImages.ColorDepth = ColorDepth.Depth32Bit
+                        'get album imagelist built first
+                        For Each album As IAlbumInfo In albums
+                            Dim albumArt As Image
+                            If (Not (album.PictureLocation Is Nothing Or album.PictureLocation = "")) Then
+                                Try
+                                    albumArt = Image.FromFile(album.PictureLocation)
+                                Catch ex As Exception
+                                    albumArt = GetDefaultAlbumImage()
+                                End Try
+                            Else
+                                albumArt = GetDefaultAlbumImage()
+                            End If
+
+                            albumImages.Images.Add(album.Name, albumArt)
+                        Next
+
+
+
+                        myParentInterface.TopAlbumsLV.LargeImageList = albumImages
+
+                        For Each album As IAlbumInfo In albums
+                            myParentInterface.TopAlbumsLV.Items.Add(album.Name, album.Name)
+                        Next
+                    End If
+                End If
+                System.Threading.Thread.Sleep(sleeptime)
+            End While
+        End Sub
+
+        Private Function GetSimilarArtistsImagesSize() As Drawing.Size
+            Dim imageWidth As UInteger = 150
+            Dim imageHeight As UInteger = imageWidth
+            Return New System.Drawing.Size(imageWidth, imageHeight)
+        End Function
+
+        Private Function GetDefaultArtistImage() As Image
+            Return New Bitmap(GetTopAlbumImagesSize().Width, GetTopAlbumImagesSize.Height) ' just a blank bitmap
+        End Function
+
+
+        Private Function GetTopAlbumImagesSize() As Drawing.Size
+            Dim imageWidth As UInteger = 90
+            Dim imageHeight As UInteger = imageWidth
+            Return New System.Drawing.Size(imageWidth, imageHeight)
+        End Function
+
+        Private Function GetDefaultAlbumImage() As Image
+            Return New Bitmap(GetTopAlbumImagesSize().Width, GetTopAlbumImagesSize.Height) ' just a blank bitmap
+        End Function
+
+        Private Sub UpdateTags()
+            While (True)
+                Dim artistInfo As IArtistInfo = Nothing
+
+                While (myTagsArtistInfoQueue.Count > 0)  'don't need/want to update for artist info object that has already expired
+                    artistInfo = myTagsArtistInfoQueue.Dequeue
+                End While
+
+                If (Not artistInfo Is Nothing) Then
+
+                    Dim tags As String() = artistInfo.Tags()
+
+
+
+                    Dim tagLabels As ArrayList = New ArrayList()
+
+                    tagLabels.Add(myParentInterface.Tag1)
+                    tagLabels.Add(myParentInterface.Tag2)
+                    tagLabels.Add(myParentInterface.Tag3)
+                    tagLabels.Add(myParentInterface.Tag4)
+                    tagLabels.Add(myParentInterface.Tag5)
+
+                    Dim index As UInteger = 0
+
+                    'initialize to be invisible
+                    For Each tagLabel As Label In tagLabels
+                        tagLabel.Visible = False
+                    Next
+
+                    If (Not tags Is Nothing) Then
+
+                        For Each tagLabel As Label In tagLabels
+                            If (index >= tags.Count) Then
+                                Exit For
+                            End If
+
+                            tagLabel.Text = tags(index)
+                            tagLabel.Visible = True
+                            index += 1
+                        Next
+                    End If
+                End If
+                System.Threading.Thread.Sleep(sleeptime)
+            End While
+        End Sub
+
+
+        Private Sub UpdateBiography()
+            While (True)
+                Dim artistInfo As IArtistInfo = Nothing
+
+                While (myBiographyArtistInfoQueue.Count > 0)  'don't need/want to update for artist info object that has already expired
+                    artistInfo = myBiographyArtistInfoQueue.Dequeue
+                End While
+
+                If (Not artistInfo Is Nothing) Then
+
+                    myParentInterface.ArtistPictureBox.ImageLocation = artistInfo.PictureLocation
+
+                    Try
+                        myParentInterface.FullBioTB.DocumentText = artistInfo.Biography
+                    Catch
+                        'usually fails because of "too busy" error"- going to ignore it for now.
+                    End Try
+
+
+                    Try
+
+                        If (Not myParentInterface.FullBioTB.Document.Body Is Nothing) Then
+                            Dim zoomLevel As Integer = 10
+                            myParentInterface.FullBioTB.Document.Body.Style = "zoom: " & zoomLevel.ToString & "%"
+                        End If
+                    Catch
+                        'not the end of the world if we just ignore it failing, for now.
+                    End Try
+                End If
+                System.Threading.Thread.Sleep(sleeptime)
+            End While
+        End Sub
+
+
+        Private Sub UpdateSimilarArtists()
+            While (True)
+                Dim artistInfo As IArtistInfo = Nothing
+
+                While (mySimilarArtistInfoQueue.Count > 0)  'don't need/want to update for artist info object that has already expired
+                    artistInfo = mySimilarArtistInfoQueue.Dequeue
+                End While
+
+                If (Not artistInfo Is Nothing) Then
+
+                    If (Not myParentInterface.SimilarArtistsLV.LargeImageList Is Nothing) Then
+
+                        For Each oldImage As Image In myParentInterface.SimilarArtistsLV.LargeImageList.Images  'clean up images first!  Otherwise- we get out of memory exceptions.
+                            oldImage.Dispose()
+                        Next
+                    End If
+
+                    myParentInterface.SimilarArtistsLV.Clear()
+
+                    If (Not artistInfo Is Nothing) Then
+                        Dim similarArtists As IArtistInfo() = artistInfo.SimilarArtists()
+
+                        If (Not similarArtists Is Nothing) Then
+
+                            Dim index As UInteger = 0
+
+
+                            Dim artistImages As ImageList = New ImageList()
+                            artistImages.ImageSize = GetSimilarArtistsImagesSize()
+                            artistImages.ColorDepth = ColorDepth.Depth32Bit
+
+                            'get album imagelist built first
+                            For Each artist As IArtistInfo In similarArtists
+                                Dim artistPicture As Image
+                                If (Not (artist.PictureLocation Is Nothing Or artist.PictureLocation = "")) Then
+                                    Try
+                                        artistPicture = Image.FromFile(artist.PictureLocation)
+                                    Catch
+                                        artistPicture = GetDefaultArtistImage()
+                                    End Try
+                                Else
+                                    artistPicture = GetDefaultArtistImage()
+                                End If
+
+
+                                artistImages.Images.Add(artist.Name, artistPicture)
+                            Next
+
+
+                            myParentInterface.SimilarArtistsLV.LargeImageList = artistImages
+
+                            For Each artist As IArtistInfo In similarArtists
+                                myParentInterface.SimilarArtistsLV.Items.Add(artist.Name, artist.Name)
+                            Next
+                        End If
+                    End If
+                End If
+                System.Threading.Thread.Sleep(sleeptime)
+            End While
+        End Sub
+    End Class
+End Class
--- IntelligentMediaPlayer/Interface/MainInterface.vb
+++ IntelligentMediaPlayer/Interface/MainInterface.vb
@@ -12,20 +12,10 @@ Imports EnvDTE80
 Public Class MainInterface
     Private player As AxWindowsMediaPlayer
     Private manager As PlaylistManager
-    Private artistInfo As IArtistInfo = Nothing
     Private myMessageFilter As MessageFilter
     Private myDTE As EnvDTE80.DTE2
+    Private myGUIAsyncUpdater As New GUIAsyncUpdater(Me)
 
-    Private Delegate_UpdateArtistInfoUI As New ASYNC_UpdateArtistInfoUI(AddressOf UpdateArtistInfoUI)
-    Private AsynchResult_UpdateArtistInfoUI As IAsyncResult = Nothing
-    Private Delegate_UpdateBiography As New ASYNCSubDelegate(AddressOf UpdateBiography)
-    Private AsynchResult_UpdateBiography As IAsyncResult = Nothing
-    Private Delegate_UpdateTags As New ASYNCSubDelegate(AddressOf UpdateTags)
-    Private AsynchResult_UpdateTags As IAsyncResult = Nothing
-    Private Delegate_UpdateSimilarArtists As New ASYNCSubDelegate(AddressOf UpdateSimilarArtists)
-    Private AsynchResult_UpdateSimilarArtists As IAsyncResult = Nothing
-    Private Delegate_UpdateTopAlbums As New ASYNCSubDelegate(AddressOf UpdateTopAlbums)
-    Private AsynchResult_UpdateTopAlbums As IAsyncResult = Nothing
 
     Private Sub Initialize() Handles Me.Load
         System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = False
@@ -150,7 +140,8 @@ Public Class MainInterface
 
     Private Sub player_CurrentItemChange(ByVal sender As Object, ByVal e As AxWMPLib._WMPOCXEvents_CurrentItemChangeEvent) Handles AxWindowsMediaPlayer1.CurrentItemChange
         ' Display the name of the new media item.
-        ArtistTextLabel.Text = player.currentMedia.getItemInfo("Artist")
+        Dim currentArtist As String = player.currentMedia.getItemInfo("Artist")
+        ArtistTextLabel.Text = player.currentMedia.getItemInfo(currentArtist)
         AlbumTextLabel.Text = player.currentMedia.getItemInfo("Album")
         TrackTextLabel.Text = player.currentMedia.getItemInfo("Title")
         AlbumYearText.Text = player.currentMedia.getItemInfo("ReleaseDateYear")
@@ -162,257 +153,11 @@ Public Class MainInterface
             PlaylistBox.SelectedIndex = currentIndex
         End If
 
-        UpdateArtistInfo()
-        UpdateAlbumInfo()
+        myGUIAsyncUpdater.AddArtistNameToQueue(currentArtist)
+       
     End Sub
 
-    Private Sub UpdateAlbumInfo()
-
-    End Sub
-
-    Private Sub UpdateArtistInfo()
-        Dim currentArtist As String = player.currentMedia.getItemInfo("Artist")
-
-        Dim needsUpdated As Boolean = False
-        If (Not artistInfo Is Nothing) Then
-            If (currentArtist.Trim.ToLower <> artistInfo.Name.Trim.ToLower) Then
-                needsUpdated = True
-            End If
-        Else
-            needsUpdated = True
-        End If
-
-        If (needsUpdated) Then
-            'need to clean up memory once in a while with all these images..
-            System.GC.Collect()
-            'ClearArtistInfoUI()
-
-            If (AsynchResult_UpdateArtistInfoUI Is Nothing) Then
-                AsynchResult_UpdateArtistInfoUI = Delegate_UpdateArtistInfoUI.BeginInvoke(currentArtist, Nothing, Nothing)
-            Else
-                Delegate_UpdateArtistInfoUI.EndInvoke(AsynchResult_UpdateArtistInfoUI)
-                AsynchResult_UpdateArtistInfoUI = Delegate_UpdateArtistInfoUI.BeginInvoke(currentArtist, Nothing, Nothing)
-            End If
-        End If
-    End Sub
-
-    Private Delegate Sub ASYNC_UpdateArtistInfoUI(ByVal currentArtist As String)
-
-
-    Private Sub UpdateArtistInfoUI(ByVal currentArtist As String)
-
-        artistInfo = New LastFMArtistInfo(currentArtist)
-        AsynchResult_UpdateBiography = Delegate_UpdateBiography.BeginInvoke(Nothing, Nothing)
-        AsynchResult_UpdateTags = Delegate_UpdateTags.BeginInvoke(Nothing, Nothing)
-        AsynchResult_UpdateSimilarArtists = Delegate_UpdateSimilarArtists.BeginInvoke(Nothing, Nothing)
-        AsynchResult_UpdateTopAlbums = Delegate_UpdateTopAlbums.BeginInvoke(Nothing, Nothing)
-
-        Delegate_UpdateTopAlbums.EndInvoke(AsynchResult_UpdateTopAlbums)
-        Delegate_UpdateBiography.EndInvoke(AsynchResult_UpdateBiography)
-        Delegate_UpdateTags.EndInvoke(AsynchResult_UpdateTags)
-        Delegate_UpdateSimilarArtists.EndInvoke(AsynchResult_UpdateSimilarArtists)
-    End Sub
-
-    Private Sub ClearArtistInfoUI()
-        FullBioTB.DocumentText = ""
-        Tag1.Text = ""
-        Tag2.Text = ""
-        Tag3.Text = ""
-        Tag4.Text = ""
-        Tag5.Text = ""
-
-        If (Not SimilarArtistsLV.LargeImageList Is Nothing) Then
-
-            For Each oldImage As Image In SimilarArtistsLV.LargeImageList.Images  'clean up images first!  Otherwise- we get out of memory exceptions.
-                oldImage.Dispose()
-            Next
-        End If
-
-        SimilarArtistsLV.Clear()
-
-        If (Not TopAlbumsLV.LargeImageList Is Nothing) Then
-            For Each oldImage As Image In TopAlbumsLV.LargeImageList.Images  'clean up images first!  Otherwise- we get out of memory exceptions.
-                oldImage.Dispose()
-            Next
-        End If
-
-        TopAlbumsLV.Clear()
-    End Sub
-
-
-    Private Delegate Sub ASYNCSubDelegate()
-
-    Private Sub UpdateTopAlbums()
-
-        If (Not TopAlbumsLV.LargeImageList Is Nothing) Then
-            For Each oldImage As Image In TopAlbumsLV.LargeImageList.Images  'clean up images first!  Otherwise- we get out of memory exceptions.
-                oldImage.Dispose()
-            Next
-        End If
-
-        TopAlbumsLV.Clear()
-
-        Dim albums As IAlbumInfo() = artistInfo.TopAlbums()
-
-        If (Not albums Is Nothing) Then
-
-            Dim albumImages As ImageList = New ImageList()
-            albumImages.ImageSize = GetTopAlbumImagesSize()
-            albumImages.ColorDepth = ColorDepth.Depth32Bit
-            'get album imagelist built first
-            For Each album As IAlbumInfo In albums
-                Dim albumArt As Image
-                If (Not (album.PictureLocation Is Nothing Or album.PictureLocation = "")) Then
-                    Try
-                        albumArt = Image.FromFile(album.PictureLocation)
-                    Catch ex As Exception
-                        albumArt = GetDefaultAlbumImage()
-                    End Try
-                Else
-                    albumArt = GetDefaultAlbumImage()
-                End If
-
-                albumImages.Images.Add(album.Name, albumArt)
-            Next
-
-
-
-            TopAlbumsLV.LargeImageList = albumImages
-
-            For Each album As IAlbumInfo In albums
-                TopAlbumsLV.Items.Add(album.Name, album.Name)
-            Next
-        End If
-    End Sub
-
-    Private Function GetSimilarArtistsImagesSize() As Drawing.Size
-        Dim imageWidth As UInteger = 150
-        Dim imageHeight As UInteger = imageWidth
-        Return New System.Drawing.Size(imageWidth, imageHeight)
-    End Function
-
-    Private Function GetDefaultArtistImage() As Image
-        Return New Bitmap(GetTopAlbumImagesSize().Width, GetTopAlbumImagesSize.Height) ' just a blank bitmap
-    End Function
-
-
-    Private Function GetTopAlbumImagesSize() As Drawing.Size
-        Dim imageWidth As UInteger = 90
-        Dim imageHeight As UInteger = imageWidth
-        Return New System.Drawing.Size(imageWidth, imageHeight)
-    End Function
-
-    Private Function GetDefaultAlbumImage() As Image
-        Return New Bitmap(GetTopAlbumImagesSize().Width, GetTopAlbumImagesSize.Height) ' just a blank bitmap
-    End Function
-
-    Private Sub UpdateTags()
-        Dim tags As String() = artistInfo.Tags()
-
-
-
-        Dim tagLabels As ArrayList = New ArrayList()
-
-        tagLabels.Add(Tag1)
-        tagLabels.Add(Tag2)
-        tagLabels.Add(Tag3)
-        tagLabels.Add(Tag4)
-        tagLabels.Add(Tag5)
-
-        Dim index As UInteger = 0
-
-        'initialize to be invisible
-        For Each tagLabel As Label In tagLabels
-            tagLabel.Visible = False
-        Next
-
-        If (Not tags Is Nothing) Then
-
-            For Each tagLabel As Label In tagLabels
-                If (index >= tags.Count) Then
-                    Exit For
-                End If
-
-                tagLabel.Text = tags(index)
-                tagLabel.Visible = True
-                index += 1
-            Next
-        End If
-    End Sub
-
-
-    Private Sub UpdateBiography()
-        ArtistPictureBox.ImageLocation = artistInfo.PictureLocation
-
-        Try
-            FullBioTB.DocumentText = artistInfo.Biography
-        Catch
-            'usually fails because of "too busy" error"- going to ignore it for now.
-        End Try
-
-
-        Try
-
-            If (Not FullBioTB.Document.Body Is Nothing) Then
-                Dim zoomLevel As Integer = 10
-                FullBioTB.Document.Body.Style = "zoom: " & zoomLevel.ToString & "%"
-            End If
-        Catch
-            'not the end of the world if we just ignore it failing, for now.
-        End Try
-    End Sub
-
-
-    Private Sub UpdateSimilarArtists()
-
-        If (Not SimilarArtistsLV.LargeImageList Is Nothing) Then
-
-            For Each oldImage As Image In SimilarArtistsLV.LargeImageList.Images  'clean up images first!  Otherwise- we get out of memory exceptions.
-                oldImage.Dispose()
-            Next
-        End If
-
-        SimilarArtistsLV.Clear()
-
-        If (Not artistInfo Is Nothing) Then
-            Dim similarArtists As IArtistInfo() = artistInfo.SimilarArtists()
-
-            If (Not similarArtists Is Nothing) Then
-
-                Dim index As UInteger = 0
-
-
-                Dim artistImages As ImageList = New ImageList()
-                artistImages.ImageSize = GetSimilarArtistsImagesSize()
-                artistImages.ColorDepth = ColorDepth.Depth32Bit
-
-                'get album imagelist built first
-                For Each artist As IArtistInfo In similarArtists
-                    Dim artistPicture As Image
-                    If (Not (artist.PictureLocation Is Nothing Or artist.PictureLocation = "")) Then
-                        Try
-                            artistPicture = Image.FromFile(artist.PictureLocation)
-                        Catch
-                            artistPicture = GetDefaultArtistImage()
-                        End Try
-                    Else
-                        artistPicture = GetDefaultArtistImage()
-                    End If
-
-
-                    artistImages.Images.Add(artist.Name, artistPicture)
-                Next
-
-
-                SimilarArtistsLV.LargeImageList = artistImages
-
-                For Each artist As IArtistInfo In similarArtists
-                    SimilarArtistsLV.Items.Add(artist.Name, artist.Name)
-                Next
-            End If
-        End If
-    End Sub
-
+   
 
     Private Sub player_CurrentPlaylistChange(ByVal sender As Object, ByVal e As AxWMPLib._WMPOCXEvents_CurrentPlaylistChangeEvent) Handles AxWindowsMediaPlayer1.CurrentPlaylistChange
 
